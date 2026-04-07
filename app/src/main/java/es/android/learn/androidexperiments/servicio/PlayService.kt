bbe9b272c7f5825f746c4324182c6ac02aaf2b4e
Manuel Vicente García Rodríguez <manuelvigarcia@hotmail.com>
2026-04-07 13:02:52 +0200

    Check point after the course.

---
+++ app/src/main/java/es/android/learn/androidexperiments/servicio/PlayService.kt
@@ -0,0 +1,12 @@
+package es.android.learn.androidexperiments.servicio
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class PlayService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
